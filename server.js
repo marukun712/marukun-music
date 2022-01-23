f1a74501ac618b0957c2e937005d9e6669007170
marukun_ <87289638+marukun712@users.noreply.github.com>
2022-01-23 15:41:17 +0900

    playlist

--- server.js
+++ server.js
@@ -6,8 +6,12 @@ const port = 3000;
 app.get("/", (req, res) =>{
     res.sendFile(__dirname + '/index.html');
     app.use(express.static('.'));
+    
 });
+
+
 
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`);
 });
+
